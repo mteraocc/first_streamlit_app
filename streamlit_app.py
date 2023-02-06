483da4e6c4d2f70aa3f27662e9ad97afb005f474
mteraocc <70817345+mteraocc@users.noreply.github.com>
2023-02-06 11:28:45 -0800

    add insert snowflake bottun, etc

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,22 +45,32 @@ try:
 except URLError as e:
   streamlit.error()
   
-#streamlit.text(fruityvice_response.json()) #just write the data to the screen
+#streamlit.stop()
 
-# write your own comment -what does the next line do? -normalize json 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list") # "" code to execute in snowflake
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake related functions
+def get_fruit_load_list():
+    with my_cur = my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list") # "" code to execute in snowflake
+        return my_cur.fetchall()
+# add a button to load the fruit
+if streamlit.button ('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+# allow the end user to add a fruit to the list
+def insert_row_snowflake (new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return 'Thanks for adding ' + new_fruit
 
-fruit_choice_2 = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.text('Thanks for adding ' + fruit_choice_2)
+fruit_choice_2 = streamlit.text_input('What fruit would you like to add?')
+                   
+if streamlit.button ('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake (fruit_choice_2)
+    streamlit.text(back_from_function)
+  
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 
