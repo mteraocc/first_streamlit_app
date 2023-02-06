6905c52c99b95eca276582139ad154a1878f8f0c
mteraocc <70817345+mteraocc@users.noreply.github.com>
2023-02-06 10:23:38 -0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,11 +42,13 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("SELECT * from fruit_load_list") # "" code to execute in snowflake
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 fruit_choice_2 = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.text('Thanks for adding ' + fruit_choice_2)
+
+my_cur.execute("insert into fruit_load_list value ('from streamlit')")
 
